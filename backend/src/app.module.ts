ebcc9958aa2b24a57f876f8c6e6bbb06b73588ac
mumbx <antonioletic@gmail.com>
2025-05-21 19:47:50 -0300

    criandos as entidades e o modelo no prisma

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TasksModule } from './tasks/tasks.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TasksModule, UsersModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
